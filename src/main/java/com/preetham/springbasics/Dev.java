c836b6e0343f24d6b7b16e3c1e0688cd382a6df7
PreethamSaiD <preetham1080@gmail.com>
2024-07-21 16:34:51 +0530

    Added a new class Machine

--- src/main/java/com/preetham/springbasics/Dev.java
+++ src/main/java/com/preetham/springbasics/Dev.java
@@ -1,10 +1,15 @@
 package com.preetham.springbasics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
+    Machine machine;
+
     public void code(){
+        machine.runCode();
         System.out.println("THis is a new project to learn Spring boot");
     }
 
---
+++ src/main/java/com/preetham/springbasics/Machine.java
@@ -0,0 +1,11 @@
+package com.preetham.springbasics;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Machine {
+
+    public void runCode(){
+        System.out.println("Code ran without errors");
+    }
+}
