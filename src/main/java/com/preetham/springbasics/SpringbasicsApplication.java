eee320cbad9585c31c509a43ded1566de775ceba
PreethamSaiD <preetham1080@gmail.com>
2024-07-21 15:55:42 +0530

    Added comments

--- src/main/java/com/preetham/springbasics/SpringbasicsApplication.java
+++ src/main/java/com/preetham/springbasics/SpringbasicsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class SpringbasicsApplication {
 
 	public static void main(String[] args) {
+		//We are getting the object created by spring fo the Dev class from the IOC container and then we are using it.
 		ApplicationContext context=SpringApplication.run(SpringbasicsApplication.class, args);
 		Dev dev=context.getBean(Dev.class);
 		dev.code();
