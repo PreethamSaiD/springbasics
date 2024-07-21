2345a4519ddad300812d94ab8452dcfcb7907995
PreethamSaiD <preetham1080@gmail.com>
2024-07-21 15:46:17 +0530

    Added the dev class and its boject is manually controlled

---
+++ src/main/java/com/preetham/springbasics/Dev.java
@@ -0,0 +1,11 @@
+package com.preetham.springbasics;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public String code(){
+        return "This is the new Spring Boot Project";
+    }
+
+}
--- src/main/java/com/preetham/springbasics/SpringbasicsApplication.java
+++ src/main/java/com/preetham/springbasics/SpringbasicsApplication.java
@@ -2,12 +2,16 @@ package com.preetham.springbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbasicsApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringbasicsApplication.class, args);
+		Dev dev=context.getBean(Dev.class);
+		dev.code();
+
 	}
 
 }
